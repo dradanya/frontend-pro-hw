5d67806f9238c778ad95089a3d7af18bcf221926
Daniel <draganov2211@gmail.com>
2024-01-24 18:37:47 +0200

    homework n5

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
-let name = prompt("Hello!, what is your name?")
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr)
 
-alert("Hello " + name + "!" + "How are you?")
+// if(numOrStr === null) {
+// console.log('ви скасували')
+// } else if( numOrStr.trim() === '' ) {
+// console.log('Empty String');
+// } else if ( isNaN( +numOrStr ) ) {
+// console.log(' number is Ba_NaN')
+// } else {
+// console.log('OK!')
+// }
+
+
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
+
+switch(true) {
+    case (numOrStr === null) :
+        console.log('You denied');
+        break;
+    case ( numOrStr.trim() === '') :
+        console.log('Empty String');
+        break;
+    case ( isNaN( +numOrStr ) ) :
+        console.log('number is Ba_Nan');
+        break;
+    default :
+    console.log('OK');
+}
