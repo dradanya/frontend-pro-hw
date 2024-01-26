24dfec75790783aeef3d611ebfd17e67979a803f
Daniel <draganov2211@gmail.com>
2024-01-26 22:18:55 +0200

    homework n8

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-let name = prompt("Hello!, what is your name?")
+const hr = +prompt('Enter the required number of hours to convert per second: ');
+const sec = hr * 3600
+	
+	if (Number.isNaN (hr)){
+		alert ('Error: You entered an additional character. Try entering only a number. Example : 5');
+		}
+	else if (!Number.isNaN (hr)) {
+		alert ('Translation result in seconds: ' + sec);
+	}
 
-alert("Hello " + name + "!" + "How are you?")
+    else{
+        alert('Error: Нou did not enter anything');
+    }
