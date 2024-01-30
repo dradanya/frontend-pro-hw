7ea8395db3560a2f7066d6dc8b237922c3cdfc03
Daniel <draganov2211@gmail.com>
2024-01-31 00:12:32 +0200

    homework n11

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-let name = prompt("Hello!, what is your name?")
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-alert("Hello " + name + "!" + "How are you?")
+function generateKey(length,characters) {
+    let result = '';
+
+    for(let i = 0; i < length;i++){
+        let num = Math.floor(Math.random() * characters.length);
+        result += characters.substring(num,num + 1);
+    }
+    return result;
+}
+
+const key = generateKey(10,characters);
+
+console.log(key);
