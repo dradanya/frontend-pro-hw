6f6ce4de0fb8f4759e2bc6f03fb6416741d90689
Daniel <draganov2211@gmail.com>
2024-02-14 22:50:02 +0200

    homework n19

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-let name = prompt("Hello!, what is your name?")
+// 1 Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (конкатенація);
 
-alert("Hello " + name + "!" + "How are you?")
+// let str1 = prompt('Enter your first string');
+// let str2 = prompt('Enter your second string');
+// let str3 = prompt('Enter your third string');
+
+// alert('Result : ' + str1 + ' ' + str2 + ' ' + str3);
+
+// 2 Розбити за цифрами п'ятизначне число і вивести у вихідному порядку через пробіл.
+
+// const number = prompt('Enter a five-digit number : ');
+
+// if (number.length !== 5 || isNaN(number)) {
+//     console.log('The entered value is not a five-digit number.')
+// } else {
+    //continue processing
+// }
+
+// const digit_5 = Math.floor(number / 10000);
+// const digit_4 = Math.floor((number % 10000) / 1000);
+// const digit_3 = Math.floor((number % 1000) / 100);
+// const digit_2 = Math.floor((number % 100) / 10);
+// const digit_1 = number % 10;
+
+// console.log('The result is :   ' + digit_5 + ' ' + digit_4 + ' '+ digit_3 + ' '+ digit_2 + ' ' + digit_1);
