fd4062d31b694acc94a310ac3d3e31b61965c43d
Daniel <draganov2211@gmail.com>
2024-02-16 19:06:04 +0200

    homework n20

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-// 1 Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (конкатенація);
+const imgArr = [
+    '1.jpg',
+    '2.jpg',
+    '3.jpg',
+    '4.jpg',
+    '5.jpg',
+    '6.jpg',
+    '7.jpg',
+    '8.jpg',
+    '9.jpg'
+]
 
-// let str1 = prompt('Enter your first string');
-// let str2 = prompt('Enter your second string');
-// let str3 = prompt('Enter your third string');
+btn.addEventListener('click' , () => {
+    randomImage();
+})
 
-// alert('Result : ' + str1 + ' ' + str2 + ' ' + str3);
+let index;
 
-// 2 Розбити за цифрами п'ятизначне число і вивести у вихідному порядку через пробіл.
+function randomImage() {
+    let randomIndex = Math.floor(Math.random() * imgArr.length);
 
-// const number = prompt('Enter a five-digit number : ');
+    selectedImage = imgArr[randomIndex];
 
-// if (number.length !== 5 || isNaN(number)) {
-//     console.log('The entered value is not a five-digit number.')
-// } else {
-    //continue processing
-// }
-
-// const digit_5 = Math.floor(number / 10000);
-// const digit_4 = Math.floor((number % 10000) / 1000);
-// const digit_3 = Math.floor((number % 1000) / 100);
-// const digit_2 = Math.floor((number % 100) / 10);
-// const digit_1 = number % 10;
-
-// console.log('The result is :   ' + digit_5 + ' ' + digit_4 + ' '+ digit_3 + ' '+ digit_2 + ' ' + digit_1);
+    document.querySelector('#randomize-image').src = 'img/' + selectedImage;
+}
