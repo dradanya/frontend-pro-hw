cfee091b4ed3d5493227501e8fdfa6707afe8216
Daniel <draganov2211@gmail.com>
2024-03-24 13:02:19 +0200

    homework n30

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-let name = prompt("Hello!, what is your name?")
+function generateList(array) {
+    return `<ul>\n${array.map(item => `    <li>${Array.isArray(item) ? generateList(item) : item}</li>`).join('\n')}\n</ul>\n`;
+}
 
-alert("Hello " + name + "!" + "How are you?")
+const array = [1, 2, 3, [1, 2, [1.1, 1.2, 1.3]], 4];
+const htmlList = generateList(array);
+console.log(htmlList);
